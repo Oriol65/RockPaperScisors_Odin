966424e00938fcd9e8021adeae21611a51c399d3
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-09-17 16:32:12 +0100

    PseudoCode

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+// Rock Paper Scissors
+    //console.log("Hello World") STEP 1
+// INPUT one of three choices STEP 3
+    // Case Insensitive
+    // Error if something else chosen, Write again
+// PC picks one of three random choices STEP 2
+    // Random Number 0-1
+    // Convert to 3 choices with equal odds
+// Compare results STEP 5
+    // Tie Scenario
+    // Win Scenario
+    // Lose Scenario
+// DECLARE scores STEP 4
+// FOR loop for 5 rounds
