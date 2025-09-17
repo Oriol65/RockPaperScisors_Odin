ddf0f4bf8880147c070e5746988f63e643072e94
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-09-17 17:56:36 +0100

    Step 6, Play to 5 completed!

--- javascript.js
+++ javascript.js
@@ -3,8 +3,9 @@
 // Compare results STEP 5
 function playRound(humanChoice, computerChoice){
     // Make it case insensitive
-    humanChoice= humanChoice.toLowerCase();
-    computerChoice=computerChoice.toLowerCase();
+    console.log(humanChoice+" "+computerChoice)
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
     // Tie scenarios
     if (humanChoice == computerChoice){
         return("Tie");
@@ -19,48 +20,60 @@ function playRound(humanChoice, computerChoice){
     }
 }
 // INPUT one of three choices STEP 3
-let getHumanChoice = prompt("Choose Rock, Paper, Scissors: ")
-console.log(getHumanChoice)
+function getHumanChoice(){
+    return(prompt("Choose Rock, Paper, Scissors: "))
+}
 
 // PC picks one of three random choices STEP 2
     // Random Number 0-1
         // Create variable to store random number from 0-1
-let randNum = .73;//Math.random();
-        // Create variable to store choice
-let getComputerChoice = "";
-    // Convert to 3 choices with equal odds
-// DECLARE scores STEP 4
-    // Variable for human score
-let humanScore = 0;
-    // Variable for computer score
-let computerScore = 0;
-if (randNum < 0.33) {
-    getComputerChoice = "Rock";
-} else if (randNum < .66) {
-    getComputerChoice = "Paper";
-} else {
-    getComputerChoice = "Scissors"
-}
-console.log(getComputerChoice)
-//console.log(getComputerChoice)
-
-let result = playRound(getHumanChoice, getComputerChoice);
-console.log(result);
-if (result=="Tie"){
-    console.log("It's a tie!");
-    console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
-} else if (result=="Win") {
-    console.log("You Win!");
-    console.log(getHumanChoice + " beats "+ getComputerChoice+"!");
-    humanScore ++;
-    console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
-} else {
-    console.log("You Lose!");
-    console.log(getComputerChoice + " beats "+ getHumanChoice+"!");
-    computerScore ++;
-    console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
+function getComputerChoice() {
+    let randNum = Math.random();
+    let choice="";
+    if (randNum < 0.33) {
+        choice="Rock";
+    } else if (randNum < .66) {
+        choice="Paper";
+    } else {
+        choice="Scissors";
+    }
+    return(choice);
 }
 
-
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    for (let i=0; i<5; i++){
+        let humanChoice=getHumanChoice();
+        let computerChoice=getComputerChoice()
+        let result = playRound(humanChoice, computerChoice);
+        console.log(result);
+        if (result=="Tie"){
+            console.log("It's a tie!");
+            console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
+        } else if (result=="Win") {
+            console.log("You Win!");
+            console.log(humanChoice + " beats "+ computerChoice+"!");
+            humanScore ++;
+            console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
+        } else {
+            console.log("You Lose!");
+            console.log(computerChoice + " beats "+ humanChoice+"!");
+            computerScore ++;
+            console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
+        }
+    }
+    console.log("The final score is: ")
+    console.log("Human: "+humanScore+" Computer:"+computerScore)
+    if (humanScore>computerScore){
+        console.log("You have Won the game, Congratulations!")
+    } else if (computerScore>humanScore) {
+        console.log("You have lost the game, try again!")
+    } else {
+        console.log("You have tied, Try again!")
+    }
+}
+playGame();
 
     // FOR loop for 5 rounds
