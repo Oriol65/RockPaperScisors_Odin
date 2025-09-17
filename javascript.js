47809f1f3391b640708214286d5b6de1db09354b
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-09-17 17:26:55 +0100

    Step 5, Single Round complete!

--- javascript.js
+++ javascript.js
@@ -1,5 +1,23 @@
 // Rock Paper Scissors
     //console.log("Hello World") STEP 1 DONE
+// Compare results STEP 5
+function playRound(humanChoice, computerChoice){
+    // Make it case insensitive
+    humanChoice= humanChoice.toLowerCase();
+    computerChoice=computerChoice.toLowerCase();
+    // Tie scenarios
+    if (humanChoice == computerChoice){
+        return("Tie");
+    } 
+    // Win scenarios
+        else if (((humanChoice=="rock")&&(computerChoice=="scissors"))||((humanChoice=="paper")&&(computerChoice=="rock"))||((humanChoice=="scissors")&&(computerChoice=="paper"))){
+        return("Win");
+    } 
+    // Everything else
+        else {
+        return("Lose");
+    }
+}
 // INPUT one of three choices STEP 3
 let getHumanChoice = prompt("Choose Rock, Paper, Scissors: ")
 console.log(getHumanChoice)
@@ -7,10 +25,15 @@ console.log(getHumanChoice)
 // PC picks one of three random choices STEP 2
     // Random Number 0-1
         // Create variable to store random number from 0-1
-let randNum = Math.random();
+let randNum = .73;//Math.random();
         // Create variable to store choice
 let getComputerChoice = "";
     // Convert to 3 choices with equal odds
+// DECLARE scores STEP 4
+    // Variable for human score
+let humanScore = 0;
+    // Variable for computer score
+let computerScore = 0;
 if (randNum < 0.33) {
     getComputerChoice = "Rock";
 } else if (randNum < .66) {
@@ -18,14 +41,26 @@ if (randNum < 0.33) {
 } else {
     getComputerChoice = "Scissors"
 }
+console.log(getComputerChoice)
 //console.log(getComputerChoice)
-// Compare results STEP 5
-    // Tie Scenario
-    // Win Scenario
-    // Lose Scenario
-// DECLARE scores STEP 4
-    // Variable for human score
-let humanScore = 0;
-    // Variable for computer score
-let computerScore = 0;
+
+let result = playRound(getHumanChoice, getComputerChoice);
+console.log(result);
+if (result=="Tie"){
+    console.log("It's a tie!");
+    console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
+} else if (result=="Win") {
+    console.log("You Win!");
+    console.log(getHumanChoice + " beats "+ getComputerChoice+"!");
+    humanScore ++;
+    console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
+} else {
+    console.log("You Lose!");
+    console.log(getComputerChoice + " beats "+ getHumanChoice+"!");
+    computerScore ++;
+    console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
+}
+
+
+
     // FOR loop for 5 rounds
