f11e16c643a6d976ecbb68edffc11e6720fe0657
Oriol65 <150459853+Oriol65@users.noreply.github.com>
2025-09-17 16:41:08 +0100

    STEP 2, Compute Choice, Done!

--- javascript.js
+++ javascript.js
@@ -1,11 +1,23 @@
 // Rock Paper Scissors
-    //console.log("Hello World") STEP 1
+    //console.log("Hello World") STEP 1 DONE
 // INPUT one of three choices STEP 3
     // Case Insensitive
     // Error if something else chosen, Write again
 // PC picks one of three random choices STEP 2
     // Random Number 0-1
+        // Create variable to store random number from 0-1
+let randNum = Math.random();
+        // Create variable to store choice
+let getComputerChoice = "";
     // Convert to 3 choices with equal odds
+if (randNum < 0.33) {
+    getComputerChoice = "Rock";
+} else if (randNum < .66) {
+    getComputerChoice = "Paper";
+} else {
+    getComputerChoice = "Scissors"
+}
+console.log(getComputerChoice)
 // Compare results STEP 5
     // Tie Scenario
     // Win Scenario
