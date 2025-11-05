14d4111cf9c24882db76c745b6d3651ff64b63b0
Oriol <150459853+Oriol65@users.noreply.github.com>
2025-11-05 18:03:52 +0000

    Cleaned JS

--- javascript.js
+++ javascript.js
@@ -1,18 +1,19 @@
 function getHumanChoice(score){
-    let choice='';
+    //Selects the buttons
     const playerRock = document.querySelector('#playerRock');
     const playerPaper = document.querySelector('#playerPaper');
     const playerScissors = document.querySelector('#playerScissors');
+    //Starts listening for clicks, then plays round with choice and score
     playerRock.onclick=()=> score= playRound('rock', score);
     playerPaper.onclick=()=> score= playRound('paper', score);
     playerScissors.onclick=()=> score=playRound('scissors', score);
-    console.log('This is the score in human choice: ' + score);
-    return(score);
 }
+
 function getComputerChoice() {
+    //random number from 0 to 1
     let randomNum = Math.random();
     let choice="";
-    // Math.random gives random number from 0-1. 1/3 and 2/3 are used to equal choices. 
+    // transforms random 0/1 to rock, paper & scissors
     if (randomNum < 1/3) {
         choice="rock";
     } else if (randomNum < 2/3) {
@@ -21,10 +22,10 @@ function getComputerChoice() {
         choice="scissors";
     }
     return(choice);
+    //Need to add a change of picture for computer choice
 }
-function playRound(humanChoice,score){
+function playRound(humanChoice, score){
     let computerChoice = getComputerChoice();
-    humanChoice = humanChoice.toLowerCase();
     let result='';
     // Game ties when both choices are equal
     if (humanChoice == computerChoice){
@@ -33,53 +34,25 @@ function playRound(humanChoice,score){
     // Human wins in the following scenarios
         else if (((humanChoice=="rock")&&(computerChoice=="scissors"))||((humanChoice=="paper")&&(computerChoice=="rock"))||((humanChoice=="scissors")&&(computerChoice=="paper"))){
             result='Win';
-            score ++;
+            score[0] ++;
     } 
     // Everything else means the user has lost
         else {
         result='Lose';
-        score --;
+        score[1] ++;
     }
-    console.log(result);
-    console.log(score);
+    //logs results. need new function here to use DOM manipulation
+    console.log('You chose: '+humanChoice+'. Your opponent chose: '+computerChoice)
+    console.log('You '+result);
+    console.log('Your score is: '+score[0]+'. Your opponents score is: '+score[1]);
     return(score);
 }
-let score=0;
-score = score+getHumanChoice(score);
 
-/*function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    for (let i=0; i<5; i++){
-        let humanChoice=getHumanChoice();
-        console.log(humanChoice);
-        let computerChoice=getComputerChoice();
-        let result = playRound(humanChoice, computerChoice);
-        console.log(result);
-        if (result=="Tie"){
-            console.log("It's a tie!");
-            console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
-        } else if (result=="Win") {
-            console.log("You Win!");
-            console.log(humanChoice + " beats "+ computerChoice+"!");
-            humanScore ++;
-            console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
-        } else {
-            console.log("You Lose!");
-            console.log(computerChoice + " beats "+ humanChoice+"!");
-            computerScore ++;
-            console.log("Your Score: "+humanScore+" Oponent's score: "+computerScore);
-        }
-    }
-    console.log("The final score is: ")
-    console.log("Human: "+humanScore+" Computer:"+computerScore)
-    if (humanScore>computerScore){
-        console.log("You have Won the game, Congratulations!")
-    } else if (computerScore>humanScore) {
-        console.log("You have lost the game, try again!")
-    } else {
-        console.log("You have tied, Try again!")
-    }
+
+function playGame(){
+let score=[humanScore=0,computerScore=0];
+console.log(getHumanChoice(score));
 }
+playGame()
 
-playGame();*/
+
