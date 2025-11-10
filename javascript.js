314d7289cbdbe4d59aafd3333dc1630e96f89937
Oriol <150459853+Oriol65@users.noreply.github.com>
2025-11-10 18:45:45 +0000

    Debug and fixed counter

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
+const computerScoreDisplay = document.querySelector("#computerScoreDisplay");
+const playerScoreDisplay = document.querySelector("#playerScoreDisplay");
+
+const playerRock = document.querySelector('#playerRock');
+const playerPaper = document.querySelector('#playerPaper');
+const playerScissors = document.querySelector('#playerScissors');
+
+const computerPicture = document.querySelector('#computerPicture');
+
+let score=[humanScore=0,computerScore=0];
+
 function getHumanChoice(score){
-    //Selects the buttons
-    const playerRock = document.querySelector('#playerRock');
-    const playerPaper = document.querySelector('#playerPaper');
-    const playerScissors = document.querySelector('#playerScissors');
     //Starts listening for clicks, then plays round with choice and score
     playerRock.onclick=()=> score= playRound('rock', score);
     playerPaper.onclick=()=> score= playRound('paper', score);
@@ -10,10 +17,8 @@ function getHumanChoice(score){
 }
 
 function getComputerChoice() {
-    //random number from 0 to 1
-    let randomNum = Math.random();
+    let randomNum = Math.random();//random number from 0 to 1
     let choice="";
-    // transforms random 0/1 to rock, paper & scissors
     if (randomNum < 1/3) {
         choice="rock";
     } else if (randomNum < 2/3) {
@@ -24,20 +29,11 @@ function getComputerChoice() {
     return(choice);
     //Need to add a change of picture for computer choice
 }
-function showScore(score,result,humanChoice,computerChoice){
-    //logs results. need new function here to use DOM manipulation
-    const roundDiv = document.createElement("div");
-    const roundResult = document.createElement("p");
-    roundResult.textContent = "You chose: "+humanChoice +".  Your oponent chose: "+computerChoice+". You "+result;
-    roundDiv.appendChild(roundResult);
-    document.querySelector(".playerChoice").after(roundDiv);
-}
 
 function playRound(humanChoice, score){
-
-    
     let computerChoice = getComputerChoice();
     let result='';
+
     // Game ties when both choices are equal
     if (humanChoice == computerChoice){
         result = 'Tie';
@@ -46,13 +42,15 @@ function playRound(humanChoice, score){
     else if (((humanChoice=="rock")&&(computerChoice=="scissors"))||((humanChoice=="paper")&&(computerChoice=="rock"))||((humanChoice=="scissors")&&(computerChoice=="paper"))){
         result='Win';
         score[0] ++;
+        playerScoreDisplay.textContent = score[0];
     } 
     // Everything else means the user has lost
         else {
         result='Lose';
         score[1] ++;
+        computerScoreDisplay.textContent = score[1];
     }
-    showScore(score,result,humanChoice,computerChoice)
+    showScore(result,humanChoice,computerChoice)
     //Create event score[0]>=3
     //Event handler => Stop game + show Win message + play again button 
     //Create event score[1]>=3
@@ -60,17 +58,14 @@ function playRound(humanChoice, score){
     //play again button. -> playGame()
     return(score);
 }
-
-function playGame(){
-let score=[humanScore=0,computerScore=0];
-    const playerResult = document.createElement("h3");
-    playerResult.textContent= score[0];
-    const computerResult = document.createElement("h3");
-    computerResult.textContent= score[1];
-    document.querySelector("div.player").appendChild(playerResult);
-    document.querySelector("div.computer").appendChild(computerResult);
-console.log(getHumanChoice(score));
+function showScore(result,humanChoice,computerChoice){
+    //logs results. need new function here to use DOM manipulation
+    const roundDiv = document.createElement("div");
+    const roundResult = document.createElement("p");
+    roundResult.textContent = "You chose: "+humanChoice +".  Your oponent chose: "+computerChoice+". You "+result;
+    roundDiv.appendChild(roundResult);
+    document.querySelector(".playerChoice").after(roundDiv);
 }
-playGame()
 
+getHumanChoice(score);
 
