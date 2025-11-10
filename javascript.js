ede833eafcca26790590ff0fe971323b103190fd
Oriol <150459853+Oriol65@users.noreply.github.com>
2025-11-10 22:33:46 +0000

    Added logic to finish game, needs polishing

--- javascript.js
+++ javascript.js
@@ -7,64 +7,97 @@ const playerScissors = document.querySelector('#playerScissors');
 
 const computerPicture = document.querySelector('#computerPicture');
 
-let score=[humanScore=0,computerScore=0];
+let score=[humanScore = 0,computerScore = 0];
 
 function getHumanChoice(score){
-    //Starts listening for clicks, then plays round with choice and score
-    playerRock.onclick=()=> score= playRound('rock', score);
-    playerPaper.onclick=()=> score= playRound('paper', score);
-    playerScissors.onclick=()=> score=playRound('scissors', score);
+
+    playerRock.onclick = () => score = playRound('rock', score);
+    playerPaper.onclick = () => score = playRound('paper', score);
+    playerScissors.onclick = () => score = playRound('scissors', score);
 }
 
 function getComputerChoice() {
     let randomNum = Math.random();//random number from 0 to 1
-    let choice="";
+    let choice = "";
     if (randomNum < 1/3) {
-        choice="rock";
+        choice = "rock";
     } else if (randomNum < 2/3) {
-        choice="paper";
+        choice = "paper";
     } else {
-        choice="scissors";
+        choice = "scissors";
     }
     return(choice);
-    //Need to add a change of picture for computer choice
+    //Need to add a change of picture for computer choice by changing source attribute, need to reset it as well at the start
+}
+
+function gameEnd(score){
+    //hide all elements before
+    playerRock.classList.add('hidden');
+    playerPaper.classList.add('hidden');
+    playerScissors.classList.add('hidden');
+    computerPicture.classList.remove('hidden')
+    
+    //roundDiv.classList.add('hidden')
+    const endDiv = document.createElement("div");
+    const endResult = document.createElement("h2");
+    const againBttn = document.createElement("button");
+    if (score[0]>score[1]){
+        endResult.textContent = "You Won!";
+    } else {
+        endResult.textContent = "You Lost!";
+    }
+
+    againBttn.textContent="Try Again?";
+    
+    endDiv.appendChild(endResult);
+    document.querySelector('.score').appendChild(endDiv);
+    document.querySelector('.score').appendChild(againBttn);
+    againBttn.onclick = () => reset();
+    //computerPicture.onclick = () => e = reset();
+}
+function reset() {
+
+    playerRock.classList.remove('hidden');
+    playerPaper.classList.remove('hidden');
+    playerScissors.classList.remove('hidden');
+    computerPicture.classList.remove('hidden')
+    playerScoreDisplay.textContent = 0;
+    computerScoreDisplay.textContent = 0;
+    getHumanChoice([0,0]);
 }
 
 function playRound(humanChoice, score){
     let computerChoice = getComputerChoice();
     let result='';
 
-    // Game ties when both choices are equal
     if (humanChoice == computerChoice){
         result = 'Tie';
-    } 
-    // Human wins in the following scenarios
-    else if (((humanChoice=="rock")&&(computerChoice=="scissors"))||((humanChoice=="paper")&&(computerChoice=="rock"))||((humanChoice=="scissors")&&(computerChoice=="paper"))){
+    } else if (((humanChoice=="rock")&&(computerChoice=="scissors"))||((humanChoice=="paper")&&(computerChoice=="rock"))||((humanChoice=="scissors")&&(computerChoice=="paper"))){
         result='Win';
         score[0] ++;
-        playerScoreDisplay.textContent = score[0];
-    } 
-    // Everything else means the user has lost
-        else {
+   
+    }   else {
         result='Lose';
         score[1] ++;
-        computerScoreDisplay.textContent = score[1];
+
     }
+    playerScoreDisplay.textContent = score[0];
+    computerScoreDisplay.textContent = score[1];
     showScore(result,humanChoice,computerChoice)
-    //Create event score[0]>=3
-    //Event handler => Stop game + show Win message + play again button 
-    //Create event score[1]>=3
-    //Event handler => Stop game + show Lose message + play again button
-    //play again button. -> playGame()
+    //Look for option to use max score instead of OR scenario
+    if (score[0]>=3||score[1]>=3){
+        gameEnd(score)
+    }
+
     return(score);
 }
 function showScore(result,humanChoice,computerChoice){
-    //logs results. need new function here to use DOM manipulation
     const roundDiv = document.createElement("div");
     const roundResult = document.createElement("p");
+
     roundResult.textContent = "You chose: "+humanChoice +".  Your oponent chose: "+computerChoice+". You "+result;
     roundDiv.appendChild(roundResult);
-    document.querySelector(".playerChoice").after(roundDiv);
+    document.querySelector(".playerChoice").after(roundDiv); //Need to see if this is where I want it or should I move it? and how to take it out?
 }
 
 getHumanChoice(score);
